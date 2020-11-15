1f75a0afdba43aadd155116b5d9af35843395253
Josh Wickster <joshwickster@gmail.com>
2020-11-15 12:58:48 -0700

    edits

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const inquirer = require('inquire');
 
 //controller logic in external forms
-const employee = require(controller/Employee.js);
-const employee = require(controller/Engineer.js);
+const Employee = require(controller/Employee.js);
+const Engineer = require(controller/Engineer.js);
+
+
+const Manager = require("Manager.js");
+const Intern = require("Intern.js");
+const htmlBlocks = require("./lib/htmlBlocks.js");
+const fs = require("fs");
+const open = require("open");
---
+++ views/HTMLheaderFile.js
@@ -0,0 +1,13 @@
+const HTMLheaderBlock = function() {
+  return
+  '<!DOCTYPE html>\n' +
+  '<html>\n' +
+  '\t<head>\n' +
+  '\t\t<meta charset="UTF-8">\n' +
+  '\t\t<title></title>\n' +
+  '\t</head>\n' +
+  '\t<body>\n' +
+  '\t\t\n' +
+  '\t</body>\n' +
+  '</html>'
+}
