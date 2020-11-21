e890c024314cf7e088bb72ccc639cdb17f1dba1d
Josh Wickster <joshwickster@gmail.com>
2020-11-21 09:41:06 -0700

    test

---
+++ app.js
@@ -0,0 +1,12 @@
+const inquirer = require('inquire');
+
+//controller logic in external forms
+const Employee = require(controller/Employee.js);
+const Engineer = require(controller/Engineer.js);
+
+
+const Manager = require("Manager.js");
+const Intern = require("Intern.js");
+const fs = require("fs");
+const open = require("open");
+const Engineer = require('./controller/Engineer');
