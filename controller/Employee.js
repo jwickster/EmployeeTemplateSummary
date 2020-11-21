8625bda34c3b0d956dc1d98674dcfbbad16ec9bf
Josh Wickster <joshwickster@gmail.com>
2020-11-21 09:49:12 -0700

    test

--- controller/Employee.js
+++ controller/Employee.js
@@ -1,1 +1,19 @@
 // TODO: Write code to define and export the Employee class
+
+//Employee class
+class Employee {
+  //contructor to instantiate name, id, email and role from class
+  constructor(name, id, email, role = "Employee"){
+    this.name = name;
+    this.id = id;
+    this.email = email;
+    this.role = role;
+  }
+  getName(){
+    return this.name;
+  }
+  
+  
+  
+  
+}
